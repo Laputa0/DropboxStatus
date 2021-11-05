82db9087c6a0bd19a50c47456590556754187a40
Laputa <1737261850@qq.com>
2021-11-05 22:17:49 +0800

    Update dbs.sh

--- dbs.sh
+++ dbs.sh
@@ -4,7 +4,7 @@ n_times=1
 while true
 do
 	c_time=$(date '+%Y-%m-%d %H:%M:%S')
-	echo "--- ${n_times}th --- ${c_time} ---"
+	echo "=== ${n_times}th === ${c_time} ==="
 	dropbox-cli status
 	sleep 1
 	((n_times++))
